15697823ca96e80db18792daf036497d6017dd97
Srikanta-ku-Mishra <srikantamishra@evawinoptimize.com>
2022-03-08 18:29:22 +0530

    Second Change

---
+++ js/Helpers/ShapeHelper.js
@@ -0,0 +1,20 @@
+
+import { TrianglePoints } from "../../css/assets/TriangleShape";
+
+export class createshape {
+    constructor(triangle, square) {
+      this.triangle() = triangle;
+      this.square() = square;
+    }
+  
+    triangle(point ,point ,point) {
+        TrianglePoints();
+     
+    }
+  
+    square(squarePoints) {
+      
+    }
+  
+  };
+  
